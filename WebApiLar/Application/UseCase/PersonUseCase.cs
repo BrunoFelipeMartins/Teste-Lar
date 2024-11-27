93081b6cba0af7161516e6b06412bc5c77818590
BrunoFelipeMartins <bruno.f.martins1996@gmail.com>
2024-11-26 23:29:02 -0300

    fixing update

--- WebApiLar/Application/UseCase/PersonUseCase.cs
+++ WebApiLar/Application/UseCase/PersonUseCase.cs
@@ -18,14 +18,14 @@ namespace WebApiLar.Application.UseCase
             if(!CpfValidator.IsValidCpf(input.cpf))
             throw new ArgumentException("CPF inválido.");
 
-            var existingPerson = personRepository.findById(input.idPerson);
-            //se pessoa igual a null e igual a 0 
-            if(existingPerson == null)
-            {
-                throw new KeyNotFoundException("Pessoa não encontrada para atualização");
-            }
+            //var existingPerson = personRepository.findById(input.idPerson);
+            //se pessoa igual a null
+            //if(existingPerson == null)
+            //{
+            //    throw new KeyNotFoundException("Pessoa não encontrada para atualização");
+           // }
 
-            Person person = new Person(input.name, input.cpf, input.dateBirth, input.active);
+            Person person = new Person(input.idPerson,input.name, input.cpf, input.dateBirth, input.active);
             person = await personRepository.save(person);
             return new PersonOutput(person.idPerson, person.name, person.cpf, person.dateBirth);
         }
